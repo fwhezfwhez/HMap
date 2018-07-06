e978f55f553803b3c02de66037d05e770033d20e
fwhez <1728565484@qq.com>
2018-07-06 11:55:54 +0800

    v1

--- example/main.go
+++ example/main.go
@@ -9,34 +9,38 @@ func main() {
 	hm := HMap.New()
 
 	//set a value with two keys
-	hm.Set("key","sub_key","value")
+	hm.Set("app_list", "cdd", "100.0.9.0")
+	hm.Set("app_list", "cdd2", "100.0.9.1")
+	hm.Set("app_list", "cdd3", "100.0.9.2")
+	hm.Set("app_list2", "cdd4", "100.0.9.2")
+	hm.Set("app_list3", "cdd5", "100.0.9.2")
+	hm.Print()
 
 	//get the value with two keys
-	v,er:=hm.Get("key","sub_key")
+	v,er:=hm.Get("app_list","cdd")
 	if er!=nil{
 		panic(er.Error())
 	}
 	fmt.Println(v)
 
 	//get values array by main key
-	vArr,er:=hm.GetByMainKey("key")
+	vArr,er:=hm.GetByMainKey("app_list")
 	if er!=nil{
 		panic(er.Error())
 	}
 	fmt.Println(vArr)
 
 	//delete values whose main key is "key"
-	hm.DeleteByMainKey("key")
+	hm.DeleteByMainKey("app_list")
+	hm.Print()
 	//delete value whose main key is "key" and sub key is "sub_key"
-	hm.Delete("key","sub_key")
+	hm.Delete("app_list2","cdd4")
+	hm.Print()
 	//delete all datas
 	hm.DeleteAll()
-
+	hm.Print()
 	//the same
 	hm.Clear()
-
-	//smart format the datas view
-	hm.Print()
 
 	//about concurrently safe
 	l:=sync.Mutex{}
