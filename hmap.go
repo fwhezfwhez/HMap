36152e7a9af2df366cb92235f57da898b691ec13
fwhez <1728565484@qq.com>
2018-10-11 15:18:34 +0800

    add context for hmap

--- hmap.go
+++ hmap.go
@@ -8,7 +8,8 @@ import (
 
 type HMap struct {
 	Content map[string]map[string]interface{}
-	M *sync.Mutex
+	Context map[string]interface{}
+	M       *sync.Mutex
 }
 
 // New a HMap
@@ -16,19 +17,20 @@ func New() *HMap {
 	tmp := make(map[string]map[string]interface{}, 0)
 	return &HMap{
 		Content: tmp,
-		M: &sync.Mutex{},
+		M:       &sync.Mutex{},
+		Context: make(map[string]interface{}, 0),
 	}
 }
 
 // Lock of the map
-func (hm *HMap) GetLock() *sync.Mutex{
+func (hm *HMap) GetLock() *sync.Mutex {
 	return hm.M
 }
-func (hm *HMap) Lock() *sync.Mutex{
+func (hm *HMap) Lock() *sync.Mutex {
 	hm.M.Lock()
 	return hm.M
 }
-func (hm *HMap) UnLock() *sync.Mutex{
+func (hm *HMap) UnLock() *sync.Mutex {
 	hm.M.Unlock()
 	return hm.M
 }
@@ -62,22 +64,22 @@ func (hm *HMap) GetByMainKey(mainKey string) ([]interface{}, error) {
 	if _, ok := hm.Content[mainKey]; !ok {
 		return nil, errors.New("no such mainKey key " + mainKey)
 	}
-	rs := make([]interface{},0)
+	rs := make([]interface{}, 0)
 	tmp := hm.Content[mainKey]
-	for _,v:=range tmp {
-		rs = append(rs,v)
+	for _, v := range tmp {
+		rs = append(rs, v)
 	}
-	return rs,nil
+	return rs, nil
 }
 
 // Delete a value indexed by main key and sub key
-func (hm *HMap) Delete(mainKey string, subKey string){
+func (hm *HMap) Delete(mainKey string, subKey string) {
 	if _, ok := hm.Content[mainKey]; !ok {
 		return
-	}else{
+	} else {
 		if _, ok2 := hm.Content[mainKey][subKey]; !ok2 {
 			return
-		}else{
+		} else {
 			delete(hm.Content[mainKey], subKey)
 		}
 	}
@@ -87,28 +89,27 @@ func (hm *HMap) Delete(mainKey string, subKey string){
 func (hm *HMap) DeleteByMainKey(mainKey string) {
 	if _, ok := hm.Content[mainKey]; !ok {
 		return
-	}else{
-		hm.Content[mainKey] = make(map[string]interface{},0)
+	} else {
+		hm.Content[mainKey] = make(map[string]interface{}, 0)
 	}
 }
 
 // Delete all datas in hm
-func (hm *HMap) DeleteAll(){
-	hm.Content = make(map[string]map[string]interface{},0)
+func (hm *HMap) DeleteAll() {
+	hm.Content = make(map[string]map[string]interface{}, 0)
 }
 
 // Clear a hm
-func (hm *HMap) Clear(){
+func (hm *HMap) Clear() {
 	hm.DeleteAll()
 }
 
-
 // Print this hm
-func (hm *HMap) Print(){
+func (hm *HMap) Print() {
 	fmt.Println("MAIN_KEY|SUB_KEY|VALUE")
-	for k1,v1:=range hm.Content{
-		for k2,v2:=range v1{
-			fmt.Println(fmt.Sprintf(" %s | %s | %v ",k1,k2,v2))
+	for k1, v1 := range hm.Content {
+		for k2, v2 := range v1 {
+			fmt.Println(fmt.Sprintf(" %s | %s | %v ", k1, k2, v2))
 		}
 	}
 }
